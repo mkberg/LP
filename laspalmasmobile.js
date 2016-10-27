e55f307490e393cd17d3e012be6c6dfe3b9a486d
mkberg <markus.berg@futurife.com>
2016-10-27 14:10:29 +0300

    initial commit

---
+++ laspalmasmobile.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+
+	var date = new Date();
+	//var dayNumber = date.getDate();
+	var dayNumber = 8; // for testing
+	selectDay(dayNumber);
+ 
+	$( "h1" ).click(function() {
+		$(this).next("div").slideToggle(200);
+		$(this).toggleClass("selected");
+	});
+
+	$( "ul.dayPicker > li" ).click(function() {
+		var day = $(this).attr("data-day");
+		selectDay(day);
+	});
+
+	function selectDay(number) {
+		$("#picker"+number).toggleClass("selected");
+		$("#picker"+number).siblings().removeClass("selected");
+		$("#day"+number).siblings().hide();
+		//$("#day"+number).slideToggle(200);
+		$("#day"+number).toggle("drop", { direction: "right" }, 100);
+	}
+
+
+});
